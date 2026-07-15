a8bd6ccad0e44417079a450cb45d903656bca35d
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2026-07-15 03:12:35 +0000

    Create habit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from './components/Header.tsx'
+import HabitForm from './components/HabitForm.tsx'
+import HabitList from './components/HabitList.tsx'
 
 export default function App() {
   return (
     <div className="max-w-2x1 mx-auto p-4 flex flex-col gap-4">
       <Header />
+      <HabitForm />
+      <HabitList />
     </div>
 
   )
---
+++ src/components/HabitList.tsx
@@ -0,0 +1,5 @@
+import Button from "./Button";
+
+export default function HabitList() {
+    return null
+}
